16686b77e6a42c6b146b509182a706f3a2368364
kubay <kubayigor@yahoo.com>
2025-06-04 00:51:44 +0300

    Добавил тесты для store

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,13 @@ def create_pet():
     response = requests.post(f'{base_url}/pet', json=payload)
     assert response.status_code == 200
     return response.json()
+
+@pytest.fixture(scope='function')
+def create_order():
+    payload = {"id": 1, "petId": 1, "quantity": 1, "status": "placed", "complete": True}
+
+    response = requests.post(f'{base_url}/store/order', json=payload)
+    response_json = response.json()
+
+    assert response.status_code == 200
+    return response_json
